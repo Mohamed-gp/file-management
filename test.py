acb41a01b4f0876d2e0f1dcefaf3417f3075508e
Mohamed-gp <mohamedterba6@gmail.com>
2024-01-09 17:06:44 +0100

    adding before_submition_dh.oy

--- test.py
+++ test.py
@@ -1,2 +1,8 @@
-name = ["john doe"]
-print(name * 10)
+def print_module_name():
+    """
+    This function prints the name of the current module.
+    """
+    print(__name__)  # Prints the name of the module where this function is defined
+
+print_module_name()  # Prints the name of the module where this function is defined
+print(__name__)  # Prints '__main__' since it is the name of the module being executed directly
